563046de8c90ea7fb869c64ef2d462518577d889
Przemysław Frąg <215719@edu.p.lodz.pl>
2020-10-20 14:59:21 +0200

    Added reversing route for app

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return ("REV API")
+    return "REV API"
+
+@app.route("/rev/<text>")
+def rev(text):
+    reversed_text = text[::-1]
+    return reversed_text
 
 app.run("192.168.122.44", 8080)
